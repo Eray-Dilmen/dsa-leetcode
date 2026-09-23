51c5de32baf4d9394c445cffadab1322331b188d
Eray Dilmen <eraydilmen5@gmail.com>
2026-09-23 12:05:21 +0300

    feat: add optimal fast and slow pointers solution for Middle of the Linked List

--- patterns/05-fast-and-slow-pointers/0876-Middle of the Linked List/solution.py
+++ patterns/05-fast-and-slow-pointers/0876-Middle of the Linked List/solution.py
@@ -4,6 +4,27 @@
 #         self.val = val
 #         self.next = next
 
+
+# Optimal Solution (Fast and Slow Pointers / One Pass)
+class Solution:
+    def middleNode(self, head: ListNode | None) -> ListNode | None:
+        slow = head
+        fast = head
+
+        while (fast and fast.next):
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
 # Brute Force Solution (Two Passes: Length + Iterate)
 class SolutionBruteForce:
     def middleNode(self, head: ListNode | None) -> ListNode | None:
@@ -15,7 +36,7 @@ class SolutionBruteForce:
             lenght += 1
             curr = curr.next
 
-        middle = lenght // 2
+        middle = lenght // 2  # tam sayı bölmesi
 
         curr = head
 
